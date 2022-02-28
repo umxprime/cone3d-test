999d587fa365355e44d2b5f6c04a9a170ab9375f
Maxime Chapelet <umxprime@videolabs.io>
2022-02-28 22:07:09 +0100

    Add animation scene and play it in scene view

--- cone3d-test/ViewController.swift
+++ cone3d-test/ViewController.swift
@@ -6,11 +6,16 @@
 //
 
 import UIKit
+import SceneKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: SCNView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let url = Bundle.main.resourceURL!.appendingPathComponent("SceneKit Asset Catalog.scnassets")
+        let scene = try? SCNScene(url: url.appendingPathComponent("cone_anim.scn"), options: nil)
+        sceneView.scene = scene
         // Do any additional setup after loading the view.
     }
 
